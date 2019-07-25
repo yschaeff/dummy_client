ca80333efe9309999b7d6168d8df221cf70b0f43
Yuri Schaeffer <yuri@nlnetlabs.nl>
2019-07-25 17:15:49 +0200

    fix device name

--- start_many.sh
+++ start_many.sh
@@ -3,7 +3,7 @@
 NAME=$(hostname)
 
 for i in {1..10}; do
-    echo starting device$i
-    python3 dummy_client.py --log-level debug --no-ssl --no-auth --host 85.214.92.70 --user device_$NAME_$i --password "super secret"  --submission-interval 30 --sample-interval 1 --downsample-interval 3 2> device_$NAME_$i.log &
+    echo starting device_${NAME}_$i
+    python3 dummy_client.py --log-level debug --no-ssl --no-auth --host 85.214.92.70 --user device_${NAME}_$i --password "super secret"  --submission-interval 30 --sample-interval 1 --downsample-interval 3 2> device_${NAME}_$i.log &
     sleep 2
 done
