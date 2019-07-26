393dfadbf3966a2f1b5c54da002c03daed1c8dd7
Yuri Schaeffer <yuri@nlnetlabs.nl>
2019-07-26 05:28:51 +0200

    collect PIDS to kill on keypress. Add check.

--- start_many.sh
+++ start_many.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
-NAME=$(hostname)
+COUNT=4
 
-for i in {1..10}; do
-    echo starting device_${NAME}_$i
+NAME=$(hostname)
+PIDS=()
+
+for ((i=0; i<COUNT; i++)); do
+    echo -n starting device_${NAME}_$i
     python3 dummy_client.py --log-level debug --no-ssl --no-auth --host 85.214.92.70 --user device_${NAME}_$i --password "super secret"  --submission-interval 30 --sample-interval 1 --downsample-interval 3 2> device_${NAME}_$i.log &
+    PID=$!
+    PIDS+="$PID "
+    echo " (PID: $PID)"
     sleep 2
 done
+
+for i in $PIDS; do
+    STATE=$(ps -q $i -o state --no-headers)
+    if [ ! "$STATE" == "S" ]; then
+        echo "WARNING. PID: $i, state: $STATE"
+    fi
+done
+
+command=' '
+while [ ! "$command" == 'k' ]; do
+    echo -n "Enter 'k' to kill all instances\$ "
+    read command
+done
+
+kill $PIDS
+echo checking all pids...
+sleep 1
+
+for i in $PIDS; do
+    STATE=$(ps -q $i -o state --no-headers)
+    if [ $STATE ]; then
+        echo "WARNING. Process $PID still running (state: $STATE)"
+    fi
+done
+echo done.
